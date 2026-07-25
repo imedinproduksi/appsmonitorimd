97ccefa42e2c6d2db13e002b9c0a58aa38c9212d
imedinproduksi <imedinproduksi@gmail.com>
2026-07-25 11:31:37 +0700

    Add Redis configuration

---
+++ src/config/redis.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+});
+
+client.on('error', (err) => {
+  console.error('Redis Client Error', err);
+});
+
+module.exports = client;
