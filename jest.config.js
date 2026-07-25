6f425fa7594f4627184c8e258423ec785d3933ba
imedinproduksi <imedinproduksi@gmail.com>
2026-07-25 11:32:59 +0700

    Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/index.js',
+    '!src/**/*.test.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+};
